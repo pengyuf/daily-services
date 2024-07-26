3dca104f3ff99210012d204a2348c65a40bb5a5b
best <2907647445@qq.com>
2024-07-26 14:23:11 +0800

    update daily controller

--- src/dailys/dailys.controller.ts
+++ src/dailys/dailys.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller('dailys')
 export class DailysController {
     @Get()
     public getDailys() {
         return `获取日记`
+    }
+
+    @Get('details')
+    public getDailyById(@Param('id') id:number){
+         return `获取日记详情`
+    }
+
+    @Post()
+    public addDaily(@Body() addDailyDto:any){
+         return `增加日记`
+    }
+
+    @Delete()
+    public delDaily(@Param('id') id:number){
+         return `删除日记`
     }
 }
