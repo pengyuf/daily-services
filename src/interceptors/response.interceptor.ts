ac0e8199349fa965d5983b32e9c1c551e94d0e61
best <2907647445@qq.com>
2024-08-07 16:42:18 +0800

    拦截器

---
+++ src/interceptors/response.interceptor.ts
@@ -0,0 +1,26 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable, map } from "rxjs";
+
+interface Response<T> {
+    data: T
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        const ctx = context.switchToHttp()
+        const request = ctx.getRequest()
+        console.log('进入全局响应拦截器')
+        return next.handle().pipe(
+            map(data => {
+                console.log('进入全局响应拦截器返回内容后')
+                return {
+                    statusCode: 0,
+                    path: request.url,
+                    message: '请求成功',
+                    data: data
+                }
+            })
+        )
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UseInterceptors } from '@nestjs/common';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './providers/users.service';
+import { ResponseInterceptor } from 'src/interceptors/response.interceptor';
 
 @Controller('users')
+@UseInterceptors(ResponseInterceptor)
 export class UsersController {
     constructor(
         private readonly usersService: UsersService
