6271c67d1449f2387a2c4f95c3db28aea5e5a5ce
best <2907647445@qq.com>
2024-08-07 17:04:31 +0800

    全局启用validationpipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true, // 将请求传入的数据，转化为OTO的实例
+    transformOptions: {
+      enableImplicitConversion: true, // pipe将数据隐式转换
+    }
+  }))
   await app.listen(3000);
 }
 bootstrap();
